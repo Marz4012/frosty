a08803d2f41a1f3390cd954696548c0b4cdf4ce4
Marz4012 <noreply>
2017-03-07 22:03:55 +0000

    Add component index.html

---
+++ ui/index.html.reel/index.html.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/index.html.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Index.html
+ * @extends Component
+ */
+exports.Index.html = Component.specialize(/** @lends Index.html# */ {
+    constructor: {
+        value: function Index.html() {
+            this.super();
+        }
+    }
+});
